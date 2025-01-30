b12e8bb1d92892b17edde46baee95008c5b5e2b8
Uomocosa <maggiori.samuele@gmail.com>
2025-01-30 11:46:57 +0100

    lesson 14

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,7 +5,7 @@ import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { 
     RevenueChartSkeleton, 
-    LatestInvoicesSkeleton,
+    // LatestInvoicesSkeleton,
     CardsSkeleton
 } from '@/app/ui/skeletons';
 
